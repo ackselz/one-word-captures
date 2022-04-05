e89ad24ca6fef322984170c310795e5d2cc83ab8
ackselsz <tanshaochong48@gmail.com>
2022-04-05 16:35:13 +0800

    build version

---
+++ build/precache-manifest.d673a716a10e2bceea91a65e98c63e76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023cf20d7de8b6266d42d8c6f8ecb898",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4dba37c01ea09d5d35e",
+    "url": "/static/css/main.95a2c5af.chunk.css"
+  },
+  {
+    "revision": "76a7bbbf9d2511fa90e1",
+    "url": "/static/js/2.edd5762d.chunk.js"
+  },
+  {
+    "revision": "68b03da80f08835f829d53eb526eecdc",
+    "url": "/static/js/2.edd5762d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4dba37c01ea09d5d35e",
+    "url": "/static/js/main.bdf55792.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
