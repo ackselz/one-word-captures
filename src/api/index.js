f558adf822dc831f4d074d185faa08f9aa07ce11
ackselz <88138479+ackselz@users.noreply.github.com>
2022-12-05 00:35:18 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://one-word-captures-backend.herokuapp.com";
+const url = "https://one-word-captures.fly.dev";
 
 axios.defaults.withCredentials = true;
 
